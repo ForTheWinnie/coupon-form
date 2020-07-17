ed7f2c3e65d350936171efdf5239b7faa059f700
ForTheWinnie <53314379+ForTheWinnie@users.noreply.github.com>
2020-07-17 07:30:32 -0700

    cleanup
    removed import App since not used in form.test & added semicolon in await

--- src/components/form/__test__/form.test.js
+++ src/components/form/__test__/form.test.js
@@ -2,15 +2,13 @@ import React from 'react';
 import { Formik, useField, Form } from 'formik'; 
 import ReactDOM from 'react-dom';
 import SubscriptionForm from './../form';
-import { render, fireEvent, wait, change } from '@testing-library/react';
-import App from './../../../App';
+import { render, fireEvent, wait } from '@testing-library/react';
 
 
 it("component renders without crashing", () => {
     const div = document.createElement("div");
     ReactDOM.render(<SubscriptionForm></SubscriptionForm>, div);
 });
-
 
 it("form renders correctly from React Testing Library", () => {
     render(<SubscriptionForm></SubscriptionForm>);
@@ -24,7 +22,7 @@ it('correct email value submitted', async () => {
     fireEvent.change(input, { target: { value: 'test@domain.com' } }); 
      
     // wrap expect in await to wait for .change
-    await wait(() => expect(input).toHaveAttribute('value', 'test@domain.com'))
+    await wait(() => expect(input).toHaveAttribute('value', 'test@domain.com'));
 });
 
 
