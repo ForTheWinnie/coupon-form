8e835c34d3415e8d18dcadea20b1804afb5d0476
ForTheWinnie <53314379+ForTheWinnie@users.noreply.github.com>
2020-07-17 13:22:25 -0700

    added form posted
    form post to fake API; added catch for error; updated comments

--- src/components/form/form.js
+++ src/components/form/form.js
@@ -3,11 +3,11 @@ import { Form, useField, Formik } from 'formik'; // used Formik for its lightwei
 import { FormColumn } from '../../Styles';
 import * as Yup from 'yup'; // used Yup for validation
 
-// for email input field
+// elements for email input field
 const EmailInput = ({ label, ...props }) => {
     const [field, meta] = useField(props);
   
-    // return component & display error if there's an error
+    // return label & input field; display error if there's an error
     return(
       <>
       <label htmlFor={props.id || props.name}>{label}</label>
@@ -22,30 +22,48 @@ const EmailInput = ({ label, ...props }) => {
 
 function SubscriptionForm () {
     return (
-        <FormColumn data-testid="EmailInput">
-                  <Formik initialValues={{email:''}} validationSchema={Yup.object({
-                    email: Yup.string()
-                      .email('Email format should be abc@domain.com')
-                      .max(50, 'Must be 50 chars or less')
-                      .required('This field is required')
-                  })}
-                  onSubmit={(values, { setSubmitting, resetForm }) => {
-                    setTimeout(() => {
-                      alert(JSON.stringify(values, null, 1));
-                      resetForm();
-                      setSubmitting(false);
-                    }, 1000);
-                  }}
-                  >
-                    {props => (
-                      <Form className="newsletterForm">
-                        <EmailInput label="Email" name="email" type="email" placeholder="abc@email.com" data-testid="test-input"></EmailInput>
-                        <button type="submit">{props.isSubmitting ? 'Joining...' : 'Join the Movement'}</button>
-                        <p className="footnote">We won't spam you. Unsubscribe at anytime.</p>
-                      </Form>
-                    )}
-                  </Formik>
-                </FormColumn>
+        <FormColumn>
+            <Formik initialValues={{email:''}} validationSchema={Yup.object({
+            email: Yup.string()
+                .email('Email format should be abc@domain.com')
+                .max(50, 'Must be 50 chars or less')
+                .required('This field is required')
+            })}
+            // POST to fake API
+            onSubmit={(values, { setSubmitting, resetForm, setStatus }) => {
+            fetch('https://jsonplaceholder.typicode.com/posts', {
+                method: 'POST',
+                headers: { "Content-type": "application/json" },
+                body: JSON.stringify({
+                    email: values.email
+                })
+            })
+            .then(response => response.json())
+            .then(json => {
+                console.log(json); // verify json (email) in console
+                const status = "200"; // in real situation, would check if (Response.status == "200")
+                if (status === "200") {
+                    //setSubmitting(true);  (this would be uncomment out in production)
+                    setSubmitting(false); // this would be deleted in production
+                    console.log("success");
+                }
+            }) 
+            .catch(error => {
+                console.error('Error:', error);
+                setSubmitting(false);
+            });
+            resetForm();
+            }}>
+                
+            {props => (
+                <Form className="newsletterForm">
+                <EmailInput label="Email" name="email" type="email" placeholder="abc@email.com" data-testid="test-input"></EmailInput>
+                <button type="submit">{props.isSubmitting ? 'Joining...' : 'Join the Movement'}</button>
+                <p className="footnote">We won't spam you. Unsubscribe at anytime.</p>
+                </Form>
+            )}
+            </Formik>
+        </FormColumn>
     )
 }
 
