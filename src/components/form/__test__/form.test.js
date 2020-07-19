bb7aa69fbf2cd8affef9d65f861fb0a609961bce
ForTheWinnie <53314379+ForTheWinnie@users.noreply.github.com>
2020-07-19 16:05:46 -0700

    form.test: DRY
    moved duplicate variables in beforeEach()

--- src/components/form/__test__/form.test.js
+++ src/components/form/__test__/form.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import SubscriptionForm from './../form';
 import { render, fireEvent, wait, cleanup, screen } from '@testing-library/react';
-import '@testing-library/jest-dom/extend-expect'
+import '@testing-library/jest-dom/extend-expect';
 
 // unmount or cleanup after every test
 afterEach(cleanup); 
@@ -21,41 +21,39 @@ describe('testing subscription form', () => {
 
 // unit tests for form validation
 describe('testing subscription form validation', () => {
-    it('correct email value submitted', async () => {
-        const handleChange = jest.fn();
-        const {getByTestId} = render(<SubscriptionForm handleChange={handleChange}/>);
-        const input = getByTestId('test-input');
-        const button = screen.getByTestId('test-button');
+    let handleChange;
+    let input;
+    let button;
 
+    // variables to run before each test
+    beforeEach(() => {
+        handleChange = jest.fn();
+        let {getByTestId} = render(<SubscriptionForm handleChange={handleChange}/>); 
+        input = screen.getByTestId('test-input');
+        button = screen.getByTestId('test-button');
+    });
+    
+    it('correct email value submitted', async () => {
         fireEvent.change(input, { target: { value: 'test@domain.com' } });
         fireEvent.click(button);
         await wait(() => expect(input).toHaveAttribute('value', 'test@domain.com'));
-        expect(screen.getByText('Joining...')).toBeInTheDocument()
+        expect(screen.getByText('Joining...')).toBeInTheDocument();
     });
     
     it('form not submitted if email field is empty', async () => {
-        const {getByTestId} = render(<SubscriptionForm />);
-        const button = screen.getByTestId('test-button');
-    
         fireEvent.click(button);
         await wait(() => expect(screen.getByText('Join the Movement')).toBeInTheDocument());
     });
 
     it('form not submitted if email format is not valid', async () => {
-        const handleChange = jest.fn();
-        const {getByTestId} = render(<SubscriptionForm handleChange={handleChange}/>);
-        const input = getByTestId('test-input');
-      
         fireEvent.change(input, { target: { value: 'testInvalidEmail' } }); 
-        await wait(() => expect(screen.getByText('Join the', { exact: false })).toBeInTheDocument());
+        await wait(() => {
+            const formButtonText = screen.getByText('Join the', { exact: false });
+            expect(formButtonText).toBeInTheDocument();
+        }); 
     });
     
     it('error (This field is required) appears if email is blank', async () => {
-        const handleChange = jest.fn();
-        const {getByTestId} = render(<SubscriptionForm handleChange={handleChange}/>);
-        const input = screen.getByTestId('test-input');
-        const button = screen.getByTestId('test-button');
-        
         fireEvent.change(input, { target: { value: '' } }); 
         fireEvent.click(button);
         await wait(() => {
@@ -65,11 +63,6 @@ describe('testing subscription form validation', () => {
     });
     
     it('error appears if email format invalid', async () => {
-        const handleChange = jest.fn();
-        const {getByTestId} = render(<SubscriptionForm handleChange={handleChange}/>);
-        const input = screen.getByTestId('test-input');
-        const button = screen.getByTestId('test-button');
-        
         fireEvent.change(input, { target: { value: 'testInvalidEmail' } }); 
         fireEvent.click(button);
         await wait(() => {
@@ -79,11 +72,6 @@ describe('testing subscription form validation', () => {
     });
     
     it('error appears if 21+ characters entered in email input', async () => {
-        const handleChange = jest.fn();
-        const {getByTestId} = render(<SubscriptionForm handleChange={handleChange}/>);
-        const input = screen.getByTestId('test-input');
-        const button = screen.getByTestId('test-button');
-        
         fireEvent.change(input, { target: { value: '1234567890123@email.com' } }); 
         fireEvent.click(button);
         await wait(() => {
